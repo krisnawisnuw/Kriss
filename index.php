df832b2cd41e29376d5bf11e1b56ded764e53f2a
krisnawisnuw <33047526+krisnawisnuw@users.noreply.github.com>
2017-10-31 11:53:10 +0700

    Update index.php

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ if($url['result'] == 404)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'Mohon Gunakan Bahasa Indonesia Yang Benar :D.'
+										'text' => 'Bott chat by : http://line.me/ti/p/~krisnawisnuw'
 									)
 							)
 						);
@@ -67,7 +67,7 @@ if($url['result'] != 100)
 							'messages' => array(
 								array(
 										'type' => 'text',					
-										'text' => 'Maaf '.$profil->displayName.' Server Kami Sedang Sibuk Sekarang.'
+										'text' => 'Maaf '.$profil->displayName.' Server kami sedang sibuk sekarang, contact my owner : http://line.me/ti/p/~krisnawisnuw.'
 									)
 							)
 						);
